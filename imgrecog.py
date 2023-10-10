7c775c5d4cefaf45631a177376f992da9354a6d3
Mustafa Avcı <123370483+lilfiro@users.noreply.github.com>
2023-10-10 16:38:00 +0300

    Model Update
    The model can receive interactive feedback from the user and update itself accurately.

--- imgrecog.py
+++ imgrecog.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import cv2
 import numpy as np
 import json
+import os
 
 # Load the trained model
 model = tf.keras.models.load_model('/home/firo/Github/Img-Recon/my_model.keras')
@@ -14,14 +15,20 @@ try:
 except FileNotFoundError:
     training_data = []
     print("Training data file not found. Initializing an empty list.")
-
+    print(training_data)  # Add this line to check the loaded data
+    
 # Interactive Feedback Loop
 while True:
-    # Prompt user to enter the image path
-    image_path = input("Enter the path of the input image: ")
-
+    # Prompt user to enter the image path or exit
+    choice = input("Enter the path of the input image or type 'exit' to quit: ")
+    
+    if choice.lower() == 'exit':
+        break  # Exit the loop if the user types 'exit'
+    
     # Capture and preprocess input image
+    image_path = choice
     input_image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+    
     if input_image is None:
         print("Invalid image path. Please try again.")
         continue
@@ -36,17 +43,20 @@ while True:
     # Display prediction
     print(f"Model predicted: {predicted_digit}")
 
-    # Get user's corrected label
+    # Check if the model's prediction is correct
     correct_label = int(input("Enter the correct digit label (0-9): "))
+    if correct_label == predicted_digit:
+        print("Model prediction is correct. No need for correction.")
+    else:
+        # Append the corrected data point to training_data
+        corrected_data_point = (input_image.tolist(), correct_label)
+        training_data.append(corrected_data_point)
 
-    # Update training data with corrected label
-    corrected_data_point = (input_image.tolist(), correct_label)
-    training_data.append(corrected_data_point)
+        # Save the updated training data to file
+        with open('/home/firo/Github/Img-Recon/training_data.json', 'w') as file:
+            json.dump(training_data, file)
+            print("Training data saved to file.")
 
-    # After loop: Save training data to file
-    with open('/home/firo/Github/Img-Recon/training_data.json', 'w') as file:
-        json.dump(training_data, file)
-        print("Training data saved to file.")
     # Reset the session and model to ensure a fresh start
     tf.keras.backend.clear_session()
 
@@ -54,9 +64,17 @@ while True:
     model = tf.keras.models.load_model('/home/firo/Github/Img-Recon/my_model.keras')
 
     # Retrain the model using updated training data
-    if training_data:
-        updated_train_labels = [label for _, label in training_data]
-        updated_train_labels = tf.keras.utils.to_categorical(updated_train_labels, num_classes=10)
-        updated_train_images = np.array([np.array(image) for image, _ in training_data])
+if training_data:
+    updated_train_labels = [label for _, label in training_data]
+    updated_train_labels = tf.keras.utils.to_categorical(updated_train_labels, num_classes=10)
+    updated_train_images = np.array([np.array(image) for image, _ in training_data])
 
-        model.fit(updated_train_images, updated_train_labels, epochs=10)
+    model.fit(updated_train_images, updated_train_labels, epochs=10)
+    
+    # Save the updated model to file
+    model.save('/home/firo/Github/Img-Recon/my_model.keras')
+    print("Updated model saved.")
+
+# Load the updated model
+model = tf.keras.models.load_model('/home/firo/Github/Img-Recon/my_model.keras')
+
